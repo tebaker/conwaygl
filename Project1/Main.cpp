896158680bf09f4a9f260c9e6e3e8226400a843f
Baker <talon.baker@atro3.astronics.com>
2020-01-22 12:06:34 -0800

    added all the components to start a projects in OpenGL. Included all the dll and h files. Included base code to get started

--- Project1/Main.cpp
+++ Project1/Main.cpp
@@ -11,76 +11,40 @@
 #include <iostream>
 #include "Header.h"
 
-// Anywhere MY_NAME is present, "Talon" will be put in it's place.
-#define MY_NAME "Talon" // Book states generally not a good practice. There are other ways to get the same result
+// Testing for OpenGL glew inclusion
+#include <glew.h>
+#include <glut.h>
 
-void doPrint()
-{
-	std::cout << "In doPrint()";
+void display() {
+	glClear(GL_COLOR_BUFFER_BIT);
+	glColor3f(1.0, 0.0, 0.0);
+
+	glBegin(GL_POINTS);
+	glVertex2f(10.0, 10.0);
+	glVertex2f(150.0, 80.0);
+	glVertex2f(100.0, 20.0);
+	glVertex2f(200.0, 100.0);
+	glEnd();
+	glFlush();
 }
 
+void myinit() {
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+	glColor3f(1.0, 0.0, 0.0);
+	glPointSize(5.0);
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	gluOrtho2D(0.0, 499.0, 0.0, 499.0);
+}
 
-int main()
-{
-	// Copy initalization of value 5 into variable width.
-	// This takes two steps: define the variable, assign the variable.
-	int width = 5;
+void main(int argc, char** argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGBA);
+	glutInitWindowSize(500, 500);
+	glutInitWindowPosition(50, 50);
+	glutCreateWindow("Points");
+	glutDisplayFunc(display);
 
-	// Direct initalization. For simple data types like integers, copy and direct initalization are essentially the same. However, direct initalization will directly assign the value 10 into height rahter than creating 10 and then assigning it to height.
-	int height(10);
-
-	// Direct brace initalization. Used when you have an object with a list of data. directly assigning 25 to depth.
-	int depth{ 25 }; // Direct brace initalization is perfered
-
-	int x{}; // Initalizes the value to 0
-
-	std::cout << "value x: " << x << std::endl;
-
-	doPrint();
-
-	std::cout << std::endl;
-
-	std::cout << "result of addition: " << add(3, 4) << std::endl;
-
-	std::cout << "Outputting results of MY_NAME: " << MY_NAME << std::endl;
-
-	std::cout << std::endl;
-	std::cout << std::endl;
-	std::cout << std::endl;
-
-	std::cout << "bool:\t\t" << sizeof(bool) << " bytes\n";
-	std::cout << "char:\t\t" << sizeof(char) << " bytes\n";
-	std::cout << "wchar_t:\t" << sizeof(wchar_t) << " bytes\n";
-	std::cout << "char16_t:\t" << sizeof(char16_t) << " bytes\n"; // C++11 only
-	std::cout << "char32_t:\t" << sizeof(char32_t) << " bytes\n"; // C++11 only
-	std::cout << "short:\t\t" << sizeof(short) << " bytes\n";
-	std::cout << "int:\t\t" << sizeof(int) << " bytes\n";
-	std::cout << "long:\t\t" << sizeof(long) << " bytes\n";
-	std::cout << "long long:\t" << sizeof(long long) << " bytes\n"; // C++11 only
-	std::cout << "float:\t\t" << sizeof(float) << " bytes\n";
-	std::cout << "double:\t\t" << sizeof(double) << " bytes\n";
-	std::cout << "long double:\t" << sizeof(long double) << " bytes\n";
-
-
-
-
-
-
-
-
-
-
-
-
-
-	// Holding for user input
-	std::cout << "Press any key to continue..." << std::endl;
-	// Reset any error flags
-	std::cin.clear();
-	// Ignores any characters in the input buffer until we find an enter character
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	// Get one more character from the user
-	std::cin.get();
-
-	return 0;
+	myinit();
+	glutMainLoop();
 }
